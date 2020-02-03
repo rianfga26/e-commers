a692be69d00a9d654f3e51df8cee3fe0449e0e98
lihamfx1 <hoacisjos48@gmail.com>
2020-02-03 14:35:56 +0700

    update ecommers laravel

---
+++ shopping-cart/database/migrations/2020_02_03_041701_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // menjalankan migrate ketika membuat tabel bernama categories
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            //JADI KITA AKAN MEMBUAT CATEGORI INI MEMILIKI ANAK KATEGORI
+            //SEHINGGA DIBUAT STRUKTUR DIMANA KATEGORI YANG MEMILIKI parent_id
+            //ADALAH KATEGORI ANAK, SEBALIKNYA JIKA KATEGORI ITU parent_id NYA NULL
+            //MAKA DIA ADALAH KATEGORI INDUK
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //METHOD INI DIGUNAKAN KETIKA ROLLBACK ATAU REFRESH DATABASE DIJALANKAN
+    public function down()
+    {
+        Schema::dropIfExists('categories'); // bagian ini untuk menghapus database diatas
+        // fungsi ini hanya akan berjalan ketika menjalankan fungsi rollback atau sejenisnya
+    }
+}
